759bcafe1f517072a51d5e864560908022f0ab11
七田　人比古 <ShichidaHitohiko@nanadenjinhiko-no-iMac.local>
2015-01-02 18:55:33 +0900

    Initial Commit

---
+++ InakadelifeRSSreader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InakadelifeRSSreader
+//
+//  Created by 七田　人比古 on 2015/01/02.
+//  Copyright (c) 2015年 Shichida Hitohiko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

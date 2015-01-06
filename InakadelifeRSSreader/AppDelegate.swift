b9e04cd9f44c11a5528fb1a52c3c6eed9fb3cce1
七田　人比古 <ShichidaHitohiko@nanadenjinhiko-no-iMac.local>
2015-01-06 23:11:01 +0900

    完成

--- InakadelifeRSSreader/AppDelegate.swift
+++ InakadelifeRSSreader/AppDelegate.swift
@@ -13,6 +13,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    var sendUrl = NSDictionary()
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
